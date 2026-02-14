427ed775f0b2a97523030da27c3df18782f6abfd
ilabeu <mionerandom55l@gmail.com>
2026-02-14 00:42:47 -0300

    Update and rename HudExample.java to Hudilabeu.java

--- src/main/java/com/example/addon/hud/Hudilabeu.java
+++ src/main/java/com/example/addon/hud/Hudilabeu.java
@@ -1,29 +1,29 @@
-package com.example.addon.hud;
+package com.ilabeu.addon.hud;
 
-import com.example.addon.AddonTemplate;
+import com.ilabeu.addon.AddonTemplate;
 import meteordevelopment.meteorclient.systems.hud.HudElement;
 import meteordevelopment.meteorclient.systems.hud.HudElementInfo;
 import meteordevelopment.meteorclient.systems.hud.HudRenderer;
 import meteordevelopment.meteorclient.utils.render.color.Color;
 
-public class HudExample extends HudElement {
+public class Hudilabeu extends HudElement {
     /**
      * The {@code name} parameter should be in kebab-case.
      */
-    public static final HudElementInfo<HudExample> INFO = new HudElementInfo<>(AddonTemplate.HUD_GROUP, "example", "HUD element example.", HudExample::new);
+    public static final HudElementInfo<Hudilabeu> INFO = new HudElementInfo<>(AddonTemplate.HUD_GROUP, "ilabeu", "HUD element ilabeu.", Hudilabeu::new);
 
-    public HudExample() {
+    public Hudilabeu() {
         super(INFO);
     }
 
     @Override
     public void render(HudRenderer renderer) {
-        setSize(renderer.textWidth("Example element", true), renderer.textHeight(true));
+        setSize(renderer.textWidth("ilabeu element", true), renderer.textHeight(true));
 
         // Render background
         renderer.quad(x, y, getWidth(), getHeight(), Color.LIGHT_GRAY);
 
         // Render text
-        renderer.text("Example element", x, y, Color.WHITE, true);
+        renderer.text("ilabeu element", x, y, Color.WHITE, true);
     }
 }
