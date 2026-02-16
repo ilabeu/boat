f42220b1bcb0158a11701d8ac94c49982aaf12f9
ilabeu <mionerandom55l@gmail.com>
2026-02-16 05:56:53 -0300

    Update AddonTemplate.java

--- src/main/java/com/ilabeu/addon/AddonTemplate.java
+++ src/main/java/com/ilabeu/addon/AddonTemplate.java
@@ -7,6 +7,7 @@ import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.commands.Commands;
 import meteordevelopment.meteorclient.systems.hud.Hud;
+import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
@@ -14,6 +15,7 @@ import org.slf4j.Logger;
 public class AddonTemplate extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
     public static final Category CATEGORY = new Category("ilabeu");
+    public static final HudGroup HUD_GROUP = new HudGroup("ilabeu");
 
     @Override
     public void onInitialize() {
