097c28f6b22819d32ef32d0b341effd192c1b2ca
ilabeu <mionerandom55l@gmail.com>
2026-02-16 06:31:27 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,38 @@
 plugins {
-    id("fabric-loom") version "1.14.10"
-    id("maven-publish")
+    id 'fabric-loom' version '1.14.10'
 }
 
-version = "0.2.0"
-group = "com.ilabeu"
-
-base {
-    archivesName.set("ilabeuhax")
-}
+group = 'com.ilabeu'
+version = '0.2.0'
 
 repositories {
-    maven("https://maven.meteordev.org/releases")
-    maven("https://maven.meteordev.org/snapshots")
+    maven { url 'https://maven.meteordev.org/releases' }
+    maven { url 'https://maven.meteordev.org/snapshots' }
     mavenCentral()
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.21.4")
-    mappings("net.fabricmc:yarn:1.21.4+build.7:v2")
-    modImplementation("net.fabricmc:fabric-loader:0.16.9")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.119.4+1.21.4")
-    modImplementation("meteordevelopment:meteor-client:0.5.10-SNAPSHOT")
+    minecraft 'com.mojang:minecraft:1.21.4'
+    mappings 'net.fabricmc:yarn:1.21.4+build.7:v2'
+    modImplementation 'net.fabricmc:fabric-loader:0.16.9'
+    modImplementation 'net.fabricmc.fabric-api:fabric-api:0.119.4+1.21.4'
+    modImplementation 'meteordevelopment:meteor-client:0.5.9'
 }
 
-tasks.processResources {
-    inputs.property("version", version)
-    filesMatching("fabric.mod.json") {
-        expand("version" to version)
+processResources {
+    inputs.property 'version', project.version
+    filteringCharset 'UTF-8'
+
+    filesMatching('fabric.mod.json') {
+        expand 'version': project.version
     }
 }
 
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-    options.release.set(21)
+tasks.withType(JavaCompile).configureEach {
+    it.options.release = 21
 }
 
 java {
-    withSourcesJar()
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
-}
-
-tasks.jar {
-    from("LICENSE") {
-        rename { "${it}_${base.archivesName.get()}" }
-    }
 }
