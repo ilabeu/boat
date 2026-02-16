bc19fe0268f5a6063bd7c2d16e77c0f21b49020b
ilabeu <mionerandom55l@gmail.com>
2026-02-16 05:57:19 -0300

    Update Exampleilabeu.java

--- src/main/java/com/ilabeu/mixin/Exampleilabeu.java
+++ src/main/java/com/ilabeu/mixin/Exampleilabeu.java
@@ -1,32 +1,16 @@
-package com.ilabeu.addon.mixin;
+package com.ilabeu.mixin;
 
 import com.ilabeu.addon.AddonTemplate;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.RunArgs;
+import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-/**
- * ilabeu Mixin class.
- * For more resources, visit:
- * <ul>
- * <li><a href="https://fabricmc.net/wiki/tutorial:mixin_introduction">The FabricMC wiki</a></li>
- * <li><a href="https://github.com/SpongePowered/Mixin/wiki">The Mixin wiki</a></li>
- * <li><a href="https://github.com/LlamaLad7/MixinExtras/wiki">The MixinExtras wiki</a></li>
- * <li><a href="https://jenkins.liteloader.com/view/Other/job/Mixin/javadoc/allclasses-noframe.html">The Mixin javadoc</a></li>
- * <li><a href="https://github.com/2xsaiko/mixin-cheatsheet">The Mixin cheatsheet</a></li>
- * </ul>
- */
-@Mixin(MinecraftClient.class)
-public abstract class ilabeuMixin {
-    /**
-     * ilabeu Mixin injection targeting the {@code <init>} method (the constructor) at {@code TAIL} (end of method).
-     */
-    @Inject(method = "<init>", at = @At("TAIL"))
-    private void onGameLoaded(RunArgs args, CallbackInfo ci) {
-        AddonTemplate.LOG.info("Hello from ilabeuMixin!");
+@Mixin(TitleScreen.class)
+public class Exampleilabeu {
+    @Inject(method = "init", at = @At("TAIL"))
+    private void onInit(CallbackInfo info) {
+        AddonTemplate.LOG.info("This line is printed by an example mixin!");
     }
 }
-
